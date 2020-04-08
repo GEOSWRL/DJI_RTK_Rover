465d5a57f4e7a3ed23e4c9eb28f3a16a145c1557
zacharymiller6 <62561639+zacharymiller6@users.noreply.github.com>
2020-04-08 12:22:28 -0600

    Add files via upload
    You will have to update the filepaths and names for each run... but other than that it should clean up the files rather efficiently!

---
+++ DJI_RTK_Rover.R
@@ -0,0 +1,37 @@
+# Script for Importing DJI RTK Rover point surveys
+rm(list = ls())
+
+library(dplyr)
+library(stringr)
+library(splitstackshape)
+
+#establish a filepath for the raw GCP file:
+Input <- "xxx.txt"
+
+#establish the column names from the collected RTK data files
+Columns <- c("altitude(m)", "antennaHeight(m)", "description", "index", "latitude", "longitude", "prefix", "stdAlt(cm)", "stdLat(cm)", "stdLng(cm)")
+
+#read in the initial textfile and clean unnessessary characters/punctuation:
+GCPraw <- readChar(Input, nchars = 2000); GCPraw
+GCP1 <- sub("rtkPoints","", GCPraw);
+GCP2 <- sub('^.......', '"', GCP1);
+GCP3 <- sub('...$', '\n', GCP2);
+GCP4 <- gsub('\\}.', '\n', GCP3);
+GCP5 <- gsub('\\{', '', GCP4); GCP5 #double check that the start and end of the string look corrected
+
+#convert to dataframe
+df <- read.csv(text = GCP5, header = FALSE, stringsAsFactors = FALSE, sep = ','); head(df)
+
+#split DJI's column headers from the columns and repopulate table with only relevant data
+df1 <- cSplit(df, splitCols = c(1:10), sep = c(":"), drop = FALSE)
+df2 <- df1 %>% select(ends_with("_2"));
+colnames(df2) <- Columns; head(df2) #reapply proper column names
+
+#reorder and remove uneccessary columns for convienience
+final <- df2[ , c("description", "antennaHeight(m)", "latitude", "longitude", "altitude(m)",
+                  "stdLat(cm)", "stdLng(cm)", "stdAlt(cm)")]
+
+#write the cleaned GCP RTK points file to the same folder in format: "YYYMMDD_GCP_clean"
+write.csv(final, file = "xxx", row.names = FALSE)
+#write the cleaned GCP RTK points file to a GCP folder in format: "YYYMMDD_GCP_clean"
+write.csv(final, file = "xxx", row.names = FALSE)
